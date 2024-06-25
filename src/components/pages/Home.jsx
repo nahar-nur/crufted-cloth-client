19e2d26f0a4bd4b1109f84b1a7667ce5795c39ae
nahar-nur <naharbb@gmail.com>
2024-06-25 13:38:54 +0600

    fetch data to implement delete function

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     const crafts = useLoaderData()
     return (
         <div className="m-20">
-            <h3 className="text-3xl text-center font-semibold mb-16">Crafts Item</h3>
+            <h3 className="text-3xl text-center font-semibold mb-16">Crafts Itemgit </h3>
             <div className="grid md:grid-cols-2 gap-4">
             {
                 crafts.map(craft=> <CraftCard
--- src/components/routes/craftCard/CraftCard.jsx
+++ src/components/routes/craftCard/CraftCard.jsx
@@ -1,22 +1,59 @@
+import Swal from "sweetalert2";
 
+const CraftCard = ({ craft }) => {
+    const { _id, name, price, image, type } = craft
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+           fetch(`http://localhost:5000/craft/${_id}`,{
+            method:'DELETE'
+           })
+           .then(res=>res.json())
+           .then(data=>{
+            console.log(data);
+            if (data.deletedCount>0)
+                   Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                    });
+           })
+            }
+          });
 
-const CraftCard = ({craft}) => {
-    const  {name,price,image,type} = craft
+    }
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl">
                 <figure>
                     <img
                         src={image}
-                    alt="Movie" />
+                        alt="Movie" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>{type}</p>
                     <p>{price}</p>
-                    
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">View Details</button>
+
+                    {/* <div className="card-actions justify-end"> */}
+                    {/* <button className="btn btn-primary">View Details</button> */}
+                    {/* </div> */}
+                    <div className="join join-vertical lg:join-horizontal bg-[#FF497C]">
+                        <button className="btn join-item bg-pink-300">View</button>
+                        <button className="btn join-item bg-sky-400">Edit</button>
+                        <button
+                        onClick={()=>handleDelete(_id)}
+                         className="btn join-item bg-yellow-300">Delete
+                         </button>
                     </div>
                 </div>
             </div>
