acc0aba96515a177c184209ee887736030ebc7b8
nahar-nur <naharbb@gmail.com>
2024-06-26 14:47:53 +0600

    added banner

---
+++ src/components/pages/Banner/Banner.jsx
@@ -0,0 +1,51 @@
+
+import slider1 from '../../../assets/textileArts/macrame1.jpg'
+import slider2 from '../../../assets/textileArts/embroidery2.jpg'
+import slider3 from '../../../assets/textileArts/beadwork1.jpg'
+import slider4 from '../../../assets/textileArts/knitting3.jpg'
+const Banner = () => {
+    return (
+        <div>
+<div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img
+      src={slider1}
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide2" className="carousel-item relative w-full">
+    <img
+    src={slider2}
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide1" className="btn btn-circle">❮</a>
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide3" className="carousel-item relative w-full">
+    <img
+      src={slider3}
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide2" className="btn btn-circle">❮</a>
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide4" className="carousel-item relative w-full">
+    <img
+      src={slider4}
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide3" className="btn btn-circle">❮</a>
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,15 @@
 
 import { useLoaderData } from "react-router-dom";
 import ItemCard from "./itemCard/ItemCard";
+import Banner from "../pages/Banner/Banner";
 
 
 const Home = () => {
     const crafts = useLoaderData()
     return (
         <div className="m-20">
-            <h3 className="text-3xl text-center font-semibold mb-16">Crafts Item </h3>
+            <Banner></Banner>
+            <h3 className="text-3xl text-center font-semibold mt-16 mb-16">Crafts Items </h3>
             <div className="grid md:grid-cols-3 gap-4">
                 {
                     crafts.map(craft=><ItemCard
